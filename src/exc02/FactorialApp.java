e92bc1df48adfb015ebc9dd0f21f17aa1457efee
ElliotUser <andersen.e@bk.ru>
2018-10-26 13:28:30 +0300

    Вычисление факториала

---
+++ src/exc02/FactorialApp.java
@@ -0,0 +1,11 @@
+package exc02;
+
+public class FactorialApp {
+    public static void main(String[] args) {
+        Factorial f = new Factorial();
+        System.out.println("Факториал 0 равен "+f.factorial(0));
+        System.out.println("Факториал 5 равен "+f.factorial(5));
+        System.out.println("Факториал 10 равен "+f.factorial(10));
+
+    }
+}
